6457dfc33fe5d4992befae72bcc0b7d555a213d0
Darren Tey <darrentey19@gmail.com>
2019-02-20 18:54:31 +0800

    nextagram templates

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+import config
+from flask import Flask
+from models.base_model import db
+
+web_dir = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), 'instagram_web')
+
+app = Flask('NEXTAGRAM', root_path=web_dir)
+
+if os.getenv('FLASK_ENV') == 'production':
+    app.config.from_object("config.ProductionConfig")
+else:
+    app.config.from_object("config.DevelopmentConfig")
+
+
+@app.before_request
+def before_request():
+    db.connect()
+
+
+@app.after_request
+def after_request(response):
+    db.close()
+    return response
